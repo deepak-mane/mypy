6fc07be68d8da2c72ed38040aa6a20f6389c751a
Deepak Mane <maildeepakmane@gmail.com>
2017-08-25 10:47:47 -0700

    Operator.py

---
+++ Operator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+# Script Name		: Operator
+# Author			: Deepak Mane
+# Created			: 8/25/2017
+# Last Modified		: 8/25/2017
+# Version			: 1.0
+# Modifications		: 1.1 - CR - Added some extra code, to check an argument is passed to the script first of all, then check it's a valid input
+# Description		: This simple script volume of sphere
+'''
+This will batch rename a group of files in a given directory,
+once you pass the current and new extensions
+'''
+__author__ = 'Deepak Mane'
+__version__ = '1.0'
+
+# Arithematic Operators
+
+num1 = 10
+num2 = 20
+print("Num1 + Num2 =", num1 + num2)
+print("Num1 - Num2 =", num1 - num2)
+print("Num1 * Num2 =", num1 * num2)
+print("Num1 / Num2 =", num1 / num2)
+print('5^3 =', 5 ** 3)
+print("20 % 3 =", 20 % 3)
+print("22//7=", 22 // 7)
