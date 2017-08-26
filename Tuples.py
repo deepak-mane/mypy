5fcb6f719807def2f08c757b9d3db96aef570373
Deepak Mane <maildeepakmane@gmail.com>
2017-08-25 22:44:58 -0700

    Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+# Script Name		: Tuples
+# Author			: Deepak Mane
+# Created			: 8/25/2017
+# Last Modified		: 8/25/2017
+# Version			: 1.0
+# Modifications		: 1.1 - CR - Added some extra code, to check an argument is passed to the script first of all, then check it's a valid input
+# Description		: This simple script volume of sphere
+'''
+This will batch rename a group of files in a given directory,
+once you pass the current and new extensions
+'''
+__author__ = 'Deepak Mane'
+__version__ = '1.0'
