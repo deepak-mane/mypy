d0e49a65ea90267d897b15c78d94eae84cc146df
deepak-mane <deepak.mane123@gmail.com>
2017-08-28 11:50:03 -0700

    Classes script added

--- function_examples.py
+++ function_examples.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-# Script Name		: Tuples
+# Script Name		: function_examples
 # Author			: Deepak Mane
-# Created			: 8/25/2017
-# Last Modified		: 8/25/2017
+# Created			: 8/26/2017
+# Last Modified		: 8/26/2017
 # Version			: 1.0
 # Modifications		: 1.1 - CR - Added some extra code, to check an argument is passed to the script first of all, then check it's a valid input
 # Description		: This simple script volume of sphere
@@ -15,3 +15,21 @@ once you pass the current and new extensions
 __author__ = 'Deepak Mane'
 __version__ = '1.0'
 
+
+def fibo(n):
+    a = 0
+    b = 1
+    for x in range(n):
+        a = b
+        b = a + b
+        print(a, '\n')
+    return b
+
+
+def main():
+    num = int(input("Enter the value of n:"))
+    print(fibo(num))
+
+
+if __name__ == '__main__':
+    main()
