565766b50e379363b53e4441cc5fec86533d6aec
deepak-mane <deepak.mane123@gmail.com>
2017-08-29 11:18:50 -0700

    Last commit

--- matplotlib_basic.py
+++ matplotlib_basic.py
@@ -15,3 +15,8 @@ once you pass the current and new extensions
 __author__ = 'Deepak Mane'
 __version__ = '1.0'
 
+from matplotlib import pyplot as plt
+# Plotting to our canvas
+plt.plot([1,2,3],[4,5,1])
+#Showing what we plotted
+plt.show()
